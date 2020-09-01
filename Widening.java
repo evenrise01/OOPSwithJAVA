8c8900448dd3743ff8669bb2181215f6b8432e0f
evenrise01 <63799338+evenrise01@users.noreply.github.com>
2020-09-01 10:20:34 +0530

    Create Widening.java

---
+++ Widening.java
@@ -0,0 +1,11 @@
+public class Myclass {
+
+    public static void main(String[] args){
+
+        int myInt = 9;
+        double myDouble = myInt; //Automatic casting: int to double
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
