486d686e51f39773d2a664d5567cf89c3a4644c7
evenrise01 <63799338+evenrise01@users.noreply.github.com>
2020-09-01 10:17:53 +0530

    Create DataImplementation.java

---
+++ DataImplementation.java
@@ -0,0 +1,27 @@
+//Write a program in Java for illustrating the concept and implementation of data types in java
+package Java;
+
+public class Java {
+    
+    public static void main (final String args[]){
+
+         float b= 3.4f;
+         float a =1.4f;
+         int mul;
+         short c = 3;
+         long l = 1458;
+         float f = 3.14f;
+         double d = 6.626789;
+         boolean bool = true;
+    }
+
+    mul= a*b; // 1*2 = 2
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(bool);
+
+}
